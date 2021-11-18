5ea845ccc6a67e1022e4ce8351531cf2fe9eefb9
molodec1337322 <sshibanov1@yandex.ru>
2021-11-18 21:47:53 +0300

    MVC

--- src/main/java/com/example/kursach2tkp/controllers/HomeController.java
+++ src/main/java/com/example/kursach2tkp/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.kursach2tkp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 public class HomeController {
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "name", required = false) String name){
+    public String sayHello(@RequestParam(value = "name", required = false) String name,
+                           Model model){
+        model.addAttribute("message", "Hello, " + name);
         System.out.println("Hello, " + name);
 
         return "home/HelloWorld";
